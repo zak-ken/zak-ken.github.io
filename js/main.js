63435ec7b4f02c515b97d9cc68db98fa396a83f1
zak-ken <zakariyahabr@gmail.com>
2019-02-27 14:50:56 +0200

    Removed uneeded js code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
     $('#main').hide()
 
-    var timerBuffer = 0
-
-    setTimeout(function() {
-        $('#intro').fadeOut(1000)
-    }, timerBuffer)
-
     setTimeout(function() {
         $('#main').fadeIn(3000)
         $('#top').hide().slideDown(3000).fadeIn(3000)
@@ -14,6 +8,6 @@ $(document).ready(function() {
             'backgroundColor': '#fff',
             'color': '#000'
         }, 500)
-    }, timerBuffer + 1000)
+    }, 1000)
 
 })
